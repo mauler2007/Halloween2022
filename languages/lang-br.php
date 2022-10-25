2ca843317938e00bdf48b0e8cd85305886248a82
andriy-lanyk <LAM1988@ukr.net>
2022-10-25 08:39:35 +0300

    Final version of compare code

---
+++ languages/lang-br.php
@@ -0,0 +1,44 @@
+<!-- Brazil content for  helloween -->
+<?php
+  $local = array(
+    "top_text" => 'rodadas grátis ou travessura!',
+    "top_text2" => 'jogue slots assustadoras e receba até',
+    "top_text3" => 'Halloween sem requisitos de aposta.',
+    "top_amount" => '<span class="bonus__sum" data-money="100">0</span> Rodadas Grátis este ',
+    "top_amount2" => '',
+    "reg_tag" => '?no_fast_reg=1',
+    "recive_bonus" => 'jogar',
+    "therms" => 'Clique aqui - se tiver coragem',
+    "rule_title" => 'Termos e Condições',
+    "rule1" => 'Esta Promoção está disponível para apostas colocadas entre as 00:00 de 29.10.2022 e as 23:59 de 31.10.2022.',
+    "rule2" => 'Os prémios são atribuídos como apostas grátis (rodadas grátis) sem quaisquer requisitos de aposta.',
+
+    "rule3" => 'O número de rodadas grátis depende do total de apostas colocadas durante o período promocional.',
+    "rule3_table_capture" => 'Os prémios são indicados na tabela:',
+    "rule3_table_sum" => 'Valor total de aposta (EUR)',
+    "rule3_table_amount" => 'Rodadas grátis',
+
+    "rule4" => 'Apenas as apostas colocadas nos jogos especificados estão incluídas na Promoção: ',
+    "rule4_table_provider" => 'Provider',
+    "rule4_table_game" => 'Game',
+    "rule4_table_operator_1" => 'Spinomenal',
+    "rule4_table_slot_1" => 'spinoween',
+    "rule4_table_operator_2" => 'InBet Games',
+    "rule4_table_slot_2" => 'walking death',
+    "rule4_table_operator_3" => 'Bgaming',
+    "rule4_table_slot_3" => 'halloween bonanza',
+    "rule4_table_operator_4" => 'Genii',
+    "rule4_table_slot_4" => 'Dead Beats',
+    "rule4_table_operator_5" => 'Endorphina',
+    "rule4_table_slot_5" => '100 zombies',
+
+    "rule5" => 'Cada cliente é elegível para um único prémio.',
+    "rule6" => 'Os prémios serão creditados no prazo de 24 horas após o término da promoção.',
+    "rule7" => 'Os prémios serão atribuídos no jogo <b>FRANKENSLOT\'S MONSTER</b> (BetSoft)',
+    "rule8" => 'Ödüllerin çevrilmesi gerekmez. Ücretsiz çevirmelerden elde edilen kazançlar anında müşterinin hesabına yatırılacaktır.',
+    "rule9" => 'Herhangi bir tartışmalı durum söz konusu olduğunda, BETANDYOU yönetiminin aldığı kararlar esas alınır.',
+    "rule10" => 'Betandyou Hüküm ve Koşulları tüm bonuslar için geçerlidir.',
+    "rule11" => 'Bu teklif yalnızca hesap bilgilerini eksiksiz doldurmuş müşteriler için geçerlidir.',
+    "rule12" => 'Tüm bonus türleri kriptodövizler için istisnasız olarak devre dışı bırakılmıştır.',
+  )
+?>
